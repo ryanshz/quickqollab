8a890e40a6aba0a5398d12b533e0a959e1ec894b
ryanshz <92750669+ryanshz@users.noreply.github.com>
2024-02-27 13:51:16 -0500

    fixed react bug causing launch issues, remade navbar/footer, changed tailwind themes

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -4,92 +4,42 @@ import logo from '../../assets/logo.png';
 
 const Navbar = () => {
 	return (
-		<header className='sticky top-0 left-0 z-50'>
-			<div class='navbar bg-base-100 '>
-				<div class='flex-none'>
-					<details class='dropdown'>
-						<summary class='m-1 btn border-green-800 rounded-md'>
-							<svg
-								xmlns='http://www.w3.org/2000/svg'
-								fill='none'
-								viewBox='0 0 24 24'
-								class='inline-block w-5 h-5 stroke-current'>
-								<path
-									stroke-linecap='round'
-									stroke-linejoin='round'
-									stroke-width='2'
-									d='M4 6h16M4 12h16M4 18h16'></path>
-							</svg>
-						</summary>
-						<ul class='p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-md w-52'>
-							<li>
-								<Link to='/' className='text-white pr-4'>
-									Home
-								</Link>
-							</li>
-							<li>
-								<Link to='/canvas' className='text-white'>
-									Canvas
-								</Link>
-							</li>
-
-							<li>
-								<Link to='/dashboard' className='text-white'>
-									Dashboard
-								</Link>
-							</li>
+		<nav className="navbar">
+			<section className='flex-none'>
+				<div className="drawer">
+					<input id="my-drawer" type="checkbox" className="drawer-toggle" />
+					<div className="drawer-content">
+						<label htmlFor="my-drawer" className="btn btn-primary drawer-button">
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+						</label>
+					</div>
+					<div className="drawer-side">
+						<label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+						<ul className="menu p-4 w-80 min-h-full bg-primary">
+							<li><Link to="/">Profile</Link></li>
+							<li><Link to="/">Customize</Link></li>
+							<li><Link to="/">Settings</Link></li>
 						</ul>
-					</details>
+					</div>
 				</div>
-				<div class='flex-1'>
-					<a class='btn btn-ghost text-xl' href='/'>
-						<img class='w-48' src={logo} alt='logo' />
-					</a>
-				</div>
-				<label class='cursor-pointer grid place-items-center pr-5'>
-					<input
-						type='checkbox'
-						value='cupcake'
-						class='toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2'
-					/>
-					<svg
-						class='col-start-1 row-start-1 stroke-base-100 fill-base-100'
-						xmlns='http://www.w3.org/2000/svg'
-						width='14'
-						height='14'
-						viewBox='0 0 24 24'
-						fill='none'
-						stroke='currentColor'
-						stroke-width='2'
-						stroke-linecap='round'
-						stroke-linejoin='round'>
-						<circle cx='12' cy='12' r='5' />
-						<path d='M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4' />
-					</svg>
-					<svg
-						class='col-start-2 row-start-1 stroke-base-100 fill-base-100'
-						xmlns='http://www.w3.org/2000/svg'
-						width='14'
-						height='14'
-						viewBox='0 0 24 24'
-						fill='none'
-						stroke='currentColor'
-						stroke-width='2'
-						stroke-linecap='round'
-						stroke-linejoin='round'>
-						<path d='M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z'></path>
-					</svg>
+			</section>
+			<section className="flex-1">
+				<Link to='/' className="mb-1 pl-2">
+					<img src={logo} alt="logo" className="w-48" />
+				</Link>
+			</section>
+			<section className="flex-none">
+				<ul className="menu menu-horizontal px-1">
+					<li><Link to='/canvas/'>canvas</Link></li>
+					<li><Link to='/dashboard/'>dashboard</Link></li>
+				</ul>
+				<label className="cursor-pointer grid place-items-center px-1">
+					<input type="checkbox" value="corporate" className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2" />
+					<svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
+					<svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
 				</label>
-				{/* <div class="avatar">
-				<div class="w-12 rounded-full">
-					<a href="{{ url_for('dashboard.dashboard')}}"><img src="static/template.jpg" /></a>
-				</div>
-			</div>  */}
-				<a href='/signup' class='pr-2'>
-					Sign Up
-				</a>
-			</div>
-		</header>
+			</section>
+		</nav>
 	);
 };
 
