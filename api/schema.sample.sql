c5471785c863340ac5424266b2524731e826ffa6
brandonhach <specialcambo@gmail.com>
2024-02-28 12:25:55 -0500

    Added comments for instructions

--- api/schema.sample.sql
+++ api/schema.sample.sql
@@ -1,3 +1,6 @@
+-- This is the schmea we will be using for development.
+-- Paste into your local postgres db (use DataGrip)
+
 -- Client (intepret as User) table
 CREATE TABLE IF NOT EXISTS client
 (
@@ -7,3 +10,11 @@ CREATE TABLE IF NOT EXISTS client
     password_hash VARCHAR(255)    NOT NULL,
     date_created  TIMESTAMP       DEFAULT CURRENT_TIMESTAMP
 );
+
+-- Dataset to populate Client table
+INSERT INTO client (username, email, password_hash) VALUES
+('ChibiCoder', 'chibicoder@techweeb.com', 'hashed_password_1'),
+('RamenDev', 'ramendev@noodlecode.com', 'hashed_password_2'),
+('HaikuHacker', 'haikuhacker@poetictech.com', 'hashed_password_3'),
+('SakuraSysadmin', 'sakurasysadmin@itblossoms.com', 'hashed_password_4'),
+('OtakuEngineer', 'otakuengineer@animeinnovation.com', 'hashed_password_5');
