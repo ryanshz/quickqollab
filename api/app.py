b64a834fb0d2f9d6d866f809b97514410669bda6
brandonhach <specialcambo@gmail.com>
2024-03-14 10:38:28 -0400

    added prefix route for each blueprint

--- api/app.py
+++ api/app.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os 
-from routes import canvas_route as cr, dashboard_route as dr, auth
+from api.routes import auth_route
+from routes import canvas_route as cr, dashboard_route as dr
 from routes.test import test_route as test
 from flask_cors import CORS
 from models.Client import db   
@@ -15,9 +16,9 @@ app = Flask(__name__, template_folder='views')
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASS")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}'
 
 # Register Blueprints
-app.register_blueprint(cr.canvas_blueprint)
-app.register_blueprint(dr.dashboard_blueprint)
-app.register_blueprint(auth.auth_blueprint)
+app.register_blueprint(cr.canvas_blueprint, url_prefix='/canvas')
+app.register_blueprint(dr.dashboard_blueprint, url_prefix='/dashboard')
+app.register_blueprint(auth_route.auth_blueprint, url_prefix='/auth')
 
 # Tests
 app.register_blueprint(test.test_blueprint)
