99d9d10f849723ac76527d15ffa546bd8425ee16
Carlos Garcia Alavez <114207613+caleaxez345@users.noreply.github.com>
2024-03-19 18:12:51 -0400

    Created Login page, need to complete displaying data on profile page after redirect

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Canvas from './pages/Canvas';
 import Index from './pages/Index';
 import Dashboard from './pages/Dashboard';
 import Footer from './components/ui/Footer';
+import Login from './pages/Auth/Login';
 import Signup from './pages/Auth/Signup';
 import Profile from './pages/Profile';
 import Test from './pages/Test';
@@ -20,6 +21,7 @@ function App() {
 					<Route path='/canvas' element={<Canvas />} />
 					<Route path='/dashboard' element={<Dashboard />} />
 					<Route path='/signup' element={<Signup />} />
+					<Route path='/login' element={<Login />} />
 					<Route path='/profile' element={<Profile />} />
 					<Route path='/test' element={<Test />} />
 				</Routes>
