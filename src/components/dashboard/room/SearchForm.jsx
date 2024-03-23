8a41d19f45e956f1573a25f5d69549c996ea415a
brandonhach <specialcambo@gmail.com>
2024-03-23 18:07:13 -0400

    added basic form for future form handling and submission

--- src/components/dashboard/room/SearchForm.jsx
+++ src/components/dashboard/room/SearchForm.jsx
@@ -2,9 +2,30 @@ import React from 'react';
 import { Search } from 'lucide-react';
 
 const SearchForm = () => {
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		try {
+			const response = await fetch('http://127.0.0.1:5000/', {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+
+			const data = await response.json();
+
+			if (response.ok) {
+			} else {
+				console.error('Login failed:', data.message);
+			}
+		} catch (error) {
+			console.error('An error occurred:', error);
+		}
+	};
 	return (
 		<div>
-			<form action=''>
+			<form action='' onSubmit={handleSubmit}>
 				<label className='input input-bordered flex items-center gap-2 rounded-t-2xl'>
 					<input type='text' className='grow' placeholder='Search for room' />
 					<button type='submit'>
