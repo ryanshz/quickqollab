34df30d786312c4e74421a9d04a748d3f6aa70a5
brandonhach <specialcambo@gmail.com>
2024-03-27 13:46:36 -0400

    basic Room model with junction Lobby

--- api/models/Room.py
+++ api/models/Room.py
@@ -2,31 +2,37 @@ from utils.sql_alchemy import db
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from datetime import datetime
+from utils.bcrypt import bcrypt
 
 Lobby = db.Table('lobby',
-    Column('id', Integer, primary_key=True),
-    Column('client_id', Integer, ForeignKey('client.id'), nullable=False),
-    Column('room_id', Integer, ForeignKey('room.id'), nullable=False),
-    Column('join_date', DateTime, default=datetime.utcnow)
+    db.Column('id', db.Integer, primary_key=True),
+    db.Column('client_id', db.Integer, db.ForeignKey('client.client_id'), nullable=False),  
+    db.Column('room_id', db.Integer, db.ForeignKey('room.room_id'), nullable=False),  
+    db.Column('join_date', db.DateTime, default=db.func.current_timestamp())
 )
 
 class Room(db.Model):
     __tablename__ = 'room'
     room_id = Column(Integer, primary_key=True)
     title = Column(String(100), nullable=False)
-    password_hash = Column(String(255), nullable=False)
+    password_hash = Column(String(255), nullable=True)
     date_created = Column(DateTime, default=datetime.utcnow)
-    host_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    host_id = db.Column(db.Integer, db.ForeignKey('client.client_id'), nullable=False)
     clients = db.relationship('Client', secondary=Lobby, back_populates='lobbies')
 
-    def create(room):
+    def save(self, password):
         try:
-            db.session.add(room)
+            if password is not None:
+                self.password_hash = bcrypt.generate_password_hash(password, 12).decode('utf-8')
+            db.session.add(self)
             db.session.commit()
-            return room, 200
+            return self, 200
         except Exception as e:
                 db.session.rollback()
                 return {"Error:": str(e)}, 500
+            
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
 
 def get_by_id(id):
     return Room.query.get(id)
