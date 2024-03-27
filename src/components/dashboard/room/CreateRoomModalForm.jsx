b847edfbb6a3c4cea5d17bc470aace0fbdb8dd1b
brandonhach <specialcambo@gmail.com>
2024-03-27 13:45:33 -0400

    refactor form to use room new route

--- src/components/dashboard/room/CreateRoomModalForm.jsx
+++ src/components/dashboard/room/CreateRoomModalForm.jsx
@@ -1,13 +1,11 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../../../middleware/AuthContext';
 
 const CreateRoomModalForm = () => {
 	const navigate = useNavigate();
-	const { user } = useAuth();
+
 	const [formData, setFormData] = useState({
 		title: '',
-		description: '',
 		password: '',
 	});
 
@@ -26,11 +24,9 @@ const CreateRoomModalForm = () => {
 			if (response.ok) {
 				navigate('/canvas');
 			} else {
-				// Handle error response
 				console.error('Failed to create room:', response.statusText);
 			}
 		} catch (error) {
-			// Handle network errors
 			console.error('Network error:', error);
 		}
 	};
@@ -49,14 +45,6 @@ const CreateRoomModalForm = () => {
 			<p className='py-4'>Enter the form below to create a public/private room.</p>
 			<div className='modal-action flex flex-col justify-center'>
 				<form method='dialog' onSubmit={handleSubmit} className='flex flex-col gap-4'>
-					<input
-						type='text'
-						name='host'
-						value={user.username}
-						onChange={handleChange}
-						className='grow'
-						hidden
-					/>
 					<label className='input input-bordered flex items-center gap-2'>
 						<input
 							type='text'
@@ -65,7 +53,8 @@ const CreateRoomModalForm = () => {
 							onChange={handleChange}
 							className='grow'
 							placeholder='Room name'
-							autocomplete='off'
+							autoComplete='off'
+							required
 						/>
 					</label>
 					<label className='input input-bordered flex items-center gap-2'>
@@ -76,7 +65,7 @@ const CreateRoomModalForm = () => {
 							onChange={handleChange}
 							className='grow'
 							placeholder='Password (optional)'
-							autocomplete='off'
+							autoComplete='off'
 						/>
 					</label>
 					{/* if there is a button in form, it will close the modal */}
