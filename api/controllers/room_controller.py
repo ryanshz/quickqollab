c4a5d534b28ba7692294d2f64745b0d5766adff2
brandonhach <specialcambo@gmail.com>
2024-03-27 13:47:03 -0400

    refactor logic to correctly create room

--- api/controllers/room_controller.py
+++ api/controllers/room_controller.py
@@ -1,36 +1,37 @@
 from models.Room import Room
+from flask import session
 
-def create_room(title, description, password):
-    try:
-        existing_room = Room.query.filter_by(title=title).first()
-        if existing_room:
-            return {"warning": "Room with this title already exists."}, 409
-            
-        new_room = Room(title=title, description=description, password=password)
-        response, status = Room.create(new_room)
+def create_room(title, password):
+    try:         
+        host_id = session.get('user_id')  
+        check_existing_room = Room.query.filter(Room.title == title).first()
+        if check_existing_room:
+            return {"warning": "Room already exists with this title name."}, 409 
+        new_room = Room(title=title, host_id=host_id)
+        response, status = new_room.save(password)
 
         if status == 200:
             return {"message": "Room created successfully"}, 200
         else:
-            return response, status
+            return response, 200
     except Exception as e:
         return {"error": str(e)}, 500
 
-def get_room_info(room_id):
-    try:
-        room = Room.query.get(room_id)
-        if room:
-            room_info = {
-                'room_id': room.room_id,
-                'title': room.title,
-                'created_by': room.created_by,
-                'description': room.description,
-                'password': room.password,
-                'is_open': room.is_open,
-                'date_created': room.date_created.strftime("%Y-%m-%d %H:%M:%S")
-            }
-            return room_info, 200
-        else:
-            return {"Error": "Room not found."}, 404
-    except Exception as e:
-        return {"Error": str(e)}, 500
+# def get_room_info(room_id):
+#     try:
+#         room = Room.query.get(room_id)
+#         if room:
+#             room_info = {
+#                 'room_id': room.room_id,
+#                 'title': room.title,
+#                 'created_by': room.created_by,
+#                 'description': room.description,
+#                 'password': room.password,
+#                 'is_open': room.is_open,
+#                 'date_created': room.date_created.strftime("%Y-%m-%d %H:%M:%S")
+#             }
+#             return room_info, 200
+#         else:
+#             return {"Error": "Room not found."}, 404
+#     except Exception as e:
+#         return {"Error": str(e)}, 500
