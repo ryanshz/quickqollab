0042ef76ebe480dd6655592b502a8356b455f605
brandonhach <specialcambo@gmail.com>
2024-03-28 23:11:07 -0400

    include credentials in header

--- src/components/auth/SignUpForm.jsx
+++ src/components/auth/SignUpForm.jsx
@@ -55,6 +55,7 @@ const SignUpForm = () => {
 						'Content-Type': 'application/json',
 					},
 					body: JSON.stringify(formData),
+					credentials: 'include',
 				});
 
 				const data = await response.json();
