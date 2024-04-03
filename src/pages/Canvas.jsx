9eb513c52540f018151e2ac4e4bc26f44c93c20e
brandonhach <specialcambo@gmail.com>
2024-04-03 12:32:40 -0400

    useState for prop between component toolbox & whiteboard. layout is conceptual atm

--- src/pages/Canvas.jsx
+++ src/pages/Canvas.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import Chatbox from '../components/canvas/Chatbox';
-import ColorBox from '../components/canvas/ColorBox';
-import ShapeBox from '../components/canvas/ShapeBox';
+import Toolbox from '../components/canvas/Toolbox';
 import Whiteboard from '../components/canvas/Whiteboard';
 import { useParams } from 'react-router-dom';
 import PasswordForm from '../components/canvas/auth/PasswordForm';
@@ -12,6 +10,7 @@ function Canvas() {
 	const location = useLocation();
 	const roomData = location.state.roomData;
 	const [color, setColor] = useState('#FFFFFF');
+	const [currentTool, setCurrentTool] = useState('scribble');
 
 	const formatDate = (dateString) => {
 		const options = { year: 'numeric', month: 'long', day: 'numeric' };
@@ -26,35 +25,14 @@ function Canvas() {
 			) : (
 				<div className='h-full w-full'>
 					<div className='flex flex-row h-full w-full'>
-						<div className='w-60'>
-							<h1 className='text-4xl'>Room details</h1>
-							<p>Testing and demonstration purposes</p>
-							<div className='divider'></div>
-							<h1>
-								Title: <span className='font-bold'>{roomData.title}</span>
-							</h1>
-							<h2>
-								room id: <span className='font-bold'>{roomData.room_id}</span>
-							</h2>
-							<h2>
-								host id: <span className='font-bold'>{roomData.host_id}</span>
-							</h2>
-							<p>
-								Date created: <span className='font-bold'>{formatDate(roomData.date_created)}</span>
-							</p>
-						</div>
 						<div className='h-full w-3/4 flex flex-col items-center justify-start gap-2 bg-base-300'>
 							{/* Whiteboard and tools */}
-							<Whiteboard setColor={color} />
 							<div className='w-2/6 h-12 flex flex-row gap-4'>
-								<ShapeBox />
-								<ColorBox setColor={setColor} />
+								<Toolbox setColor={setColor} setCurrentTool={setCurrentTool}></Toolbox>
 							</div>
+							<Whiteboard penColor={color} currentTool={currentTool} />
 						</div>
-						<div className='h-full w-1/4'>
-							{/* Chatbox */}
-							<Chatbox />
-						</div>
+						<div className='h-full w-1/4'></div>
 					</div>
 				</div>
 			)}
