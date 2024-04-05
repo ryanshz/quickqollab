c3712e14e1787ea3c79df96799994ded4e4dc41f
brandonhach <specialcambo@gmail.com>
2024-04-04 21:20:03 -0400

    wrap content with CanvasProvider for whiteboard context

--- src/pages/Canvas.jsx
+++ src/pages/Canvas.jsx
@@ -6,6 +6,7 @@ import { useParams } from 'react-router-dom';
 import PasswordForm from '../components/canvas/auth/PasswordForm';
 import Chatbox from '../components/canvas/Chatbox';
 import { MessageSquareMore } from 'lucide-react';
+import { CanvasProvider } from '../components/canvas/context/CanvasContext';
 
 function Canvas() {
 	const [color, setColor] = useState('#FFFFFF');
@@ -16,24 +17,27 @@ function Canvas() {
 			{false ? (
 				<PasswordForm />
 			) : (
-				<div className='h-full w-full  bg-base-100'>
-					<Whiteboard penColor={color} currentTool={currentTool} />
-					<div className='flex flex-row justify-center '>
-						<Toolbox setColor={setColor} setCurrentTool={setCurrentTool} className='z-50'></Toolbox>
-						{/* {Chatbox Modal (opens chatbox)} */}
-						<div className='dropdown dropdown-top'>
-							<div
-								tabindex='0'
-								role='button'
-								className='btn m-1 tooltip tooltip-bottom flex flex-row items-center justify-center bg-base-200'
-								data-tip='Message'>
-								<button>
-									<MessageSquareMore />
-								</button>
+				<CanvasProvider>
+					<div className='h-full w-full  bg-base-100'>
+						<Whiteboard penColor={color} currentTool={currentTool} />
+						<div className='flex flex-row justify-center '>
+							<Toolbox setColor={setColor} setCurrentTool={setCurrentTool} className='z-50'></Toolbox>
+							{/* {Chatbox Modal (opens chatbox)} */}
+							<></>
+							<div className='dropdown dropdown-top'>
+								<div
+									tabindex='0'
+									role='button'
+									className='btn m-1 tooltip tooltip-bottom flex flex-row items-center justify-center bg-base-200'
+									data-tip='Message'>
+									<button>
+										<MessageSquareMore />
+									</button>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
+				</CanvasProvider>
 			)}
 		</main>
 	);
