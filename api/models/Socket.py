c57c192af2bae771693941bcfeb00105e56c2044
brandonhach <specialcambo@gmail.com>
2024-04-20 10:45:25 -0400

    basic socket functions but not working

--- api/models/Socket.py
+++ api/models/Socket.py
@@ -1,19 +1,27 @@
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room, leave_room
+from models.Room import Room
+from models.Client import Client
+from flask import session
 
 socketio = SocketIO()
 
-@socketio.on('connect')
-def handle_connect():
-    print('Client connected')
+@socketio.on('create_room')
+def handle_create_room(data):
+    room_id = data.get('room_id')
+    title = data.get('title')
+    password = data.get('password', '')
 
-@socketio.on('disconnect')
-def handle_disconnect():
-    print('Client disconnected')
-
-@socketio.on('chat_message')
-def handle_chat_message(message):
-    emit('chat_message', message, broadcast=True)
+    room = Room.query.filter_by(room_id=room_id).first()
+    if room:
+        emit('socketResponse', {'success': True, 'room_id': room_id}, broadcast=True)
+    else:
+        emit('socketResponse', {'success': False, 'message': 'Room creation failed'})
+        
+@socketio.on('join_room')
+def handle_join_room(data):
+    room_id = data.get('room_id')
+    join_room(room_id)
     
-@socketio.on('drawLine')
-def handle_draw_line(data):
-    emit('drawLine', data, broadcast=True)
+    emit('room_joined', {'success': True, 'message': f'Joined room successfully', 'room_id': room_id}, room=room_id)
+    emit('room_update', {'message': 'A new user has joined the room.'}, room=room_id, broadcast=True)
+
