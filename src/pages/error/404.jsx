b5c4f470215e2fd7bf7579ea43dd67514eed037c
brandonhach <specialcambo@gmail.com>
2024-04-24 15:45:45 -0400

    fix text color visibility

--- src/pages/error/404.jsx
+++ src/pages/error/404.jsx
@@ -7,7 +7,7 @@ const NotFound404 = () => {
 			<div className='text-center flex flex-col items-center justify-center gap-2'>
 				<h1 className='text-9xl font-black text-gray-200 dark:text-gray-700'>404</h1>
 				<p className='text-2xl font-bold tracking-tight text-primary-content sm:text-4xl '>Uh-oh!</p>
-				<p className='px-4 text-neutral-content'>We can't find that page. 😔</p>
+				<p className='px-4 text-neutral'>We can't find that page. 😔</p>
 				<div className='rounded-2xl'>
 					<ReactPlayer url='https://www.youtube.com/watch?v=h2Gw01Gl40o' playing={true} />
 				</div>
